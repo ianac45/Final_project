28c69ede651dec9fd9ee450c960ae2d81e5ba960
ianac45 <icohen2025@kehillahstudent.org>
2023-05-15 11:59:44 -0700

    Updated

---
+++ final_project.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+url = "https://en.wikipedia.org/wiki/Demographics_of_the_world"
+dfs = pd.read_html(url,)
+#looks for the third graph 
+df = dfs[3]
+
+# removes all spaces from the colum so it can be entered into a graph
+df["World population (in thousands)"] = df["World population (in thousands)"].str.replace(' ', '')
+#plots these colums into the graph 
+df = df[["Year", "World population (in thousands)"]].astype("int")
+# defines the x and y axis 
+df.plot(x = "Year", y = "World population (in thousands)")
+# creates a bar graph 
+df.plot()
+plt.show()
+
+# prints the info being graphed 
+print(df.info())
